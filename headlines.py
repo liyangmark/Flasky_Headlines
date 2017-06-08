9fe65c757e06588f62f6ddb66507a20dd0f5bbea
liyangmark <desdevl@foxmail.com>
2017-06-08 11:56:24 +0800

    make this rss more interesting which can show more with Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -21,16 +21,22 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route('/<publication>')
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_artice = feed['entries'][10]
-    return """<html>
-      <body>
-          <h1> BBC Headlines </h1>
-          <b>{0}</b> <br/>
-          <i>{1}</i> <br/>
-          <p>{2}</p> <br/>
-      </body>
-    </html>
-    """.format(first_artice.get("title"), first_artice.get("published"), first_artice.get("summary"))
+    # first_artice = feed['entries'][0]
+    # return """<html>
+    #   <body>
+    #       <h1> BBC Headlines </h1>
+    #       <b>{0}</b> <br/>
+    #       <i>{1}</i> <br/>
+    #       <p>{2}</p> <br/>
+    #   </body>
+    # </html>
+    # """.format(first_artice.get("title"), first_artice.get("published"), first_artice.get("summary"))
+    # return render_template('home.html',
+    #                        title=first_artice.get('title'),
+    #                        published=first_artice.get('published'),
+    #                        summary=first_artice.get('summary'))
+    # return render_template('home.html', article=first_artice)
+    return render_template('home.html', articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
