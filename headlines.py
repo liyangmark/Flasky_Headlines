b31f91c80e92c568686c92b3ca4870a61a7d1e28
liyangmark <desdevl@foxmail.com>
2017-06-09 11:10:42 +0800

    add query of weather and currency. and use urllib, json, response, cookie(make_response) to give more power. use css to make the page looks better

--- headlines.py
+++ headlines.py
@@ -1,5 +1,10 @@
 import feedparser
 from flask import Flask, render_template, request
+import json
+import urllib
+import os
+import datetime
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -8,41 +13,79 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             # 'fox': 'http://feeds.foxnews.com/foxnews/latest',
             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-# @app.route('/')
-# @app.route('/bbc')
-# def bbc():
-#     return get_news('bbc')
-#
-# @app.route('/iol')
-# def cnn():
-#     return get_news('iol')
+DEFAULTS = {'publication': 'bbc',
+            'city': 'London, UK',
+            'currency_from': 'GBP',
+            'currency_to': 'USD'}
+WEATHER_URL =  'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
+CURRENCY_URL = 'https://openexchangerates.org//api/latest.json?app_id={}'
+
 
 @app.route('/')
-def get_news():
-    query = request.args.get("publication")
+def home():    
+    publication = get_value_with_fallback('publication')
+    articles = get_news(publication)
+
+    city = get_value_with_fallback('city')
+    weather = get_weather(city)
+
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
+    rate, currencies = get_rate(currency_from, currency_to)
+
+    response = make_response(render_template("home.html",
+        articles=articles, 
+        weather=weather, 
+        currency_from=currency_from, 
+        currency_to=currency_to, 
+        rate=rate,
+        currencies=sorted(currencies)))
+
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = 'bbc'
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
 
-    # first_artice = feed['entries'][0]
-    # return """<html>
-    #   <body>
-    #       <h1> BBC Headlines </h1>
-    #       <b>{0}</b> <br/>
-    #       <i>{1}</i> <br/>
-    #       <p>{2}</p> <br/>
-    #   </body>
-    # </html>
-    # """.format(first_artice.get("title"), first_artice.get("published"), first_artice.get("summary"))
-    # return render_template('home.html',
-    #                        title=first_artice.get('title'),
-    #                        published=first_artice.get('published'),
-    #                        summary=first_artice.get('summary'))
-    # return render_template('home.html', article=first_artice)
-    return render_template('home.html', articles=feed['entries'])
+def get_weather(query):
+    query = urllib.request.quote(query)
+    url = WEATHER_URL.format(query, os.environ.get('WEATHER_KEY'))
+    # print(url)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                   parsed["weather"][0]["description"],
+                   "temperature":parsed["main"]['temp'],
+                   "city":parsed["name"],
+                   "country":parsed["sys"]["country"]}
+        return weather
+
+def get_rate(frm, to):
+    all_currency = urllib.request.urlopen(CURRENCY_URL.format(os.environ.get('EXCHANGE_KEY'))).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate/frm_rate, parsed.keys())
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
